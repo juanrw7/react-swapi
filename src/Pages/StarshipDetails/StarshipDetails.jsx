1042027eb86e242f10aa5afaac9ed8c80b2d502f
juanrw <juan.rw24@gmail.com>
2024-05-03 18:37:33 -0500

    make details visible

--- src/Pages/StarshipDetails/StarshipDetails.jsx
+++ src/Pages/StarshipDetails/StarshipDetails.jsx
@@ -6,6 +6,9 @@ import { Link } from 'react-router-dom'
 // services 
 import { getStarshipDetails } from '../../assets/services/sw-api'
 
+// css
+import "./StarshipDetails.css"
+
 const StarshipDetails = () => {
   const [starshipDetails, setStarshipsDetails] = useState({})
   const {starShipId} = useParams()
